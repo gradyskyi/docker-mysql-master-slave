2a99a0ab6fd4aa6c05da2b61bec3fb4ac548fe7a
roman.gradyskiy <roman.gradyskiy@skylum.com>
2021-09-23 19:41:33 +0300

    two replicas

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 docker-compose down
 rm -rf ./master/data/*
-rm -rf ./slave/data/*
+rm -rf ./slave/data_1/*
+rm -rf ./slave/data_2/*
 docker-compose build
 docker-compose up -d
 
@@ -15,9 +16,15 @@ done
 priv_stmt='GRANT REPLICATION SLAVE ON *.* TO "mydb_slave_user"@"%" IDENTIFIED BY "mydb_slave_pwd"; FLUSH PRIVILEGES;'
 docker exec mysql_master sh -c "export MYSQL_PWD=111; mysql -u root -e '$priv_stmt'"
 
-until docker-compose exec mysql_slave sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
+until docker-compose exec mysql_slave_1 sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
 do
-    echo "Waiting for mysql_slave database connection..."
+    echo "Waiting for mysql_slave_1 database connection..."
+    sleep 4
+done
+
+until docker-compose exec mysql_slave_2 sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
+do
+    echo "Waiting for mysql_slave_2 database connection..."
     sleep 4
 done
 
@@ -33,6 +40,18 @@ start_slave_stmt="CHANGE MASTER TO MASTER_HOST='$(docker-ip mysql_master)',MASTE
 start_slave_cmd='export MYSQL_PWD=111; mysql -u root -e "'
 start_slave_cmd+="$start_slave_stmt"
 start_slave_cmd+='"'
-docker exec mysql_slave sh -c "$start_slave_cmd"
+docker exec mysql_slave_1 sh -c "$start_slave_cmd"
+docker exec mysql_slave_2 sh -c "$start_slave_cmd"
 
-docker exec mysql_slave sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+docker exec mysql_slave_1 sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+docker exec mysql_slave_2 sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+
+docker exec -i mysql_master sh -c "mysql -uroot -p111 mydb -e 'DROP TABLE cameras;'";
+docker exec -i mysql_master sh -c "mysql -uroot -p111 mydb -e 'CREATE TABLE cameras (id int(11) NOT NULL AUTO_INCREMENT, brand varchar(32) NOT NULL DEFAULT \"\", model varchar(255) NOT NULL DEFAULT \"\", alt_search_words varchar(1500) NOT NULL DEFAULT \"\", PRIMARY KEY (id), KEY brand (brand)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT=\"all cameras\";'";
+
+for i in {1..100}
+do
+  echo "inserted $i cameras"
+  docker exec -i mysql_master sh -c "mysql -uroot -p111 mydb -e 'INSERT INTO cameras VALUES ($i,\"canon\",\"Canon\",0);'";
+  sleep 1
+done
